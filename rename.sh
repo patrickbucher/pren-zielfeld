8037c74207c03d99370ba1b5c0340e10fec7dbd7
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-03-18 10:51:30 +0100

    quite decent version

--- rename.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-n=1
-for f in `ls -tr *.jpg`; do
-    name="`printf '%.2d' $n`"
-    echo $name
-    mv "$f" "$name".jpg
-    n=`expr $n + 1`
-done
--- square.py
+++ square.py
@@ -7,6 +7,15 @@ import sys
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 
+def center(cont):
+    cx, cy = -1, -1
+    m = cv2.moments(cont)
+    m00 = m['m00']
+    if m00 > 0:
+        cx = int(m['m10']/m00)
+        cy = int(m['m01']/m00)
+    return (cx, cy)
+
 def process(filename):
     image_bgr = cv2.imread(filename)
     image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
@@ -18,8 +27,10 @@ def process(filename):
             cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     hierarchy = hierarchy[0]
-    w, h = threshold.shape
-    max_area = w * h * 0.75
+    height, width = threshold.shape
+    img_mid = int(height/2)
+    cv2.line(image_rgb, (0, img_mid), (width, img_mid), (255,0,255), 5)
+    max_area = width * height * 0.75
     hi = 0
     for cont in contours:
         hier = hierarchy[hi]
@@ -29,17 +40,41 @@ def process(filename):
         area = cv2.contourArea(cont)
         if area > max_area or area < 500:
             continue
-        m = cv2.moments(cont)
         (x, y, w, h) = cv2.boundingRect(approx)
+        (cx, cy) = center(cont)
         ar = w / float(h)
         color = (0, 255, 0) # default: green
+        innermost = False
         if hier[2] < 0:
             color = (0, 0, 255) # innermost: blue
+            innermost = True
         if hier[3] < 0:
             color = (255, 0, 0) # outermost: red
         if ar >= 0.85 and ar <= 1.15:
             cont = cont.astype('int')
             cv2.drawContours(image_rgb, [cont], -1, color, 10)
+            if innermost:
+                (cx, cy) = center(cont)
+                dist = height/2 - cy
+                print('distance={}px'.format(dist))
+                if cx > 0 and cy > 0:
+                    cv2.circle(image_rgb, (cx, cy), 10, (255, 255, 0), 5)
+                    if height*0.5 > dist >= height*0.3:
+                        print('far away')
+                    elif height*0.3 > dist >= height*0.2:
+                        print('away')
+                    elif height*0.2 > dist >= height*0.1:
+                        print('close')
+                    elif height*0.1 > dist >= height*0.05:
+                        print('closer')
+                    elif height*0.05 > dist >= height*0.01:
+                        print('very close')
+                    elif height*0.01 > dist >= 0:
+                        print('extremely close')
+                    elif dist < 0:
+                        print('over')
+                    elif dist > 0:
+                        print('not there yet')
 
     plt.imshow(image_rgb)
     plt.show()
